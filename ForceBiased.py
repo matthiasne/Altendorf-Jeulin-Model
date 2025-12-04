a8824ecca15d7ca838a3ae7ff3e7aeab383dd217
Alex Keilmann <keilmann@rptu.de>
2025-12-04 16:17:03 +0100

    RSA-like displacement should be part of the fiber model generation

--- ForceBiased.py
+++ ForceBiased.py
@@ -22,7 +22,6 @@ def run_force_biased(fs: list[list[Ball]], image_size: tuple[int, int, int],
     max_radius = max(ball.radius for fiber in fs for ball in fiber)
     min_radius = min(ball.radius for fiber in fs for ball in fiber)
 
-    # TODO: displacement similar to RSA
     grid = sh.SpatialHashing(image_size, 2.5 * max_radius)
     grid.add_fiber_system(fs)
     force_strength, overlap = calculate_forces(grid, fiber_system=fs)
