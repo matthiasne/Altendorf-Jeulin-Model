4aaf81c410a97fd084fdae9b99df574fa4c6c41e
Alex Keilmann <keilmann@rptu.de>
2026-05-29 15:38:12 +0200

    merge SpatialHashing.py with endlessFibers

--- src/Altendorf_Jeulin_Model/SpatialHashing.py
+++ src/Altendorf_Jeulin_Model/SpatialHashing.py
@@ -2,7 +2,6 @@ from math import floor, ceil
 from itertools import product
 import numpy as np
 from Altendorf_Jeulin_Model.Fiber import Ball, Fiber
-from line_profiler import profile
 
 
 class SpatialHashing:
@@ -23,7 +22,6 @@ class SpatialHashing:
         The cell size in each direction x, y, and z
     """
 
-    @profile
     def __init__(self, image_size: tuple[int, int, int], max_cell_size: int):
         """
         Initializes the SpatialHashing object, i.e. a grid for spatial hashing
@@ -42,7 +40,6 @@ class SpatialHashing:
         self.cells = [[] for _ in range(self.n_cells)]
         self.cell_width = tuple(ceil(size / div) for size, div in zip(image_size, self.division))
 
-    @profile
     def _in_bounds(self, index: tuple[int, int, int]) -> bool:
         """
         test whether an index is within bounds of the SpatialHashing
@@ -56,8 +53,7 @@ class SpatialHashing:
         """
         return all(0 <= c < m for c, m in zip(index, self.division))
 
-    @profile
-    def get_cell_index_of_coord(self, position: np.ndarray) -> tuple[int, int, int]:
+    def get_cell_index_of_coord(self, position: np.ndarray, is_periodic: bool = True) -> tuple[int, int, int]:
         """
         gets the cell index of a specific coordinate
 
@@ -68,13 +64,15 @@ class SpatialHashing:
         :return: tuple[int, int, int]
             The cell index
         """
-
-        pos_mod = (position[0] % self.image_size[0], position[1] % self.image_size[1],
-                   position[2] % self.image_size[2])
+        if is_periodic:
+            pos_mod = (position[0] % self.image_size[0], position[1] % self.image_size[1],
+                       position[2] % self.image_size[2])
+        else:
+            pos_mod = (position[0], position[1], position[2])
         return tuple(floor(coord / width) for coord, width in zip(pos_mod, self.cell_width))
 
-    @profile
-    def get_neighbor_cell_indices(self, index: tuple[int, int, int]) -> list[tuple[int, int, int]]:
+    def get_neighbor_cell_indices(self, index: tuple[int, int, int], is_periodic: bool = True) -> list[
+        tuple[int, int, int]]:
         """
         Gets the indices of all neighboring cells
         :param index: tuple[int, int, int]
@@ -89,14 +87,17 @@ class SpatialHashing:
         for di, dj, dk in product((-1, 0, 1), repeat=3):
             if di == dj == dk == 0:
                 continue  # skip the cell itself
-            neighbor = ((index[0] + di) % self.division[0], (index[1] + dj) % self.division[1],
-                        (index[2] + dk) % self.division[2])
+            if is_periodic:
+                neighbor = ((index[0] + di) % self.division[0], (index[1] + dj) % self.division[1],
+                            (index[2] + dk) % self.division[2])
+            else:
+                neighbor = ((index[0] + di), (index[1] + dj), (index[2] + dk))
             if self._in_bounds(neighbor) and neighbor not in neighbor_cells:
                 neighbor_cells.append(neighbor)
         return neighbor_cells
 
-    @profile
-    def get_younger_neighbor_cell_indices(self, index: tuple[int, int, int]):
+    def get_younger_neighbor_cell_indices(self, index: tuple[int, int, int], is_periodic: bool = True) -> list[
+        tuple[int, int, int]]:
         """
         Gets the indices of all neighboring cells
         :param index: tuple[int, int, int]
@@ -108,13 +109,12 @@ class SpatialHashing:
         neighbor_cells = set()
         for di, dj, dk in product((-1, 0, 1), repeat=3):
             if (di, dj, dk) < (0, 0, 0):
-                neighbor = ((index[0] + di) % self.division[0] + ((index[1] + dj) % self.division[1])*self.division[0]
-                            + ((index[2] + dk) % self.division[2])*self.division[0]*self.division[1])
+                neighbor = ((index[0] + di) % self.division[0] + ((index[1] + dj) % self.division[1]) * self.division[0]
+                            + ((index[2] + dk) % self.division[2]) * self.division[0] * self.division[1])
                 neighbor_cells.add(neighbor)
         return neighbor_cells
 
-    @profile
-    def add_ball(self, ball: Ball):
+    def add_ball(self, ball: Ball, is_periodic: bool = True):
         """
         Adds a ball to the SpatialHashing
 
@@ -124,12 +124,14 @@ class SpatialHashing:
             The ball that is to be added
         """
         coord = ball.coordinate
-        index = self.get_cell_index_of_coord(coord % self.image_size)
+        if is_periodic:
+            index = self.get_cell_index_of_coord(coord % self.image_size)
+        else:
+            index = self.get_cell_index_of_coord(coord)
         idx = index[0] + index[1] * self.division[0] + index[2] * self.division[0] * self.division[1]
         self.cells[idx].append(ball)
 
-    @profile
-    def add_fiber(self, fiber: Fiber):
+    def add_fiber(self, fiber: Fiber, is_periodic: bool = True):
         """
         Adds a fiber to the SpatialHashing
 
@@ -139,10 +141,9 @@ class SpatialHashing:
             The fiber to be added
         """
         for ball in fiber.balls:
-            self.add_ball(ball)
+            self.add_ball(ball, is_periodic)
 
-    @profile
-    def add_fiber_system(self, fiber_system: list[Fiber]):
+    def add_fiber_system(self, fiber_system: list[Fiber], is_periodic: bool = True):
         """
         Adds a fiber system to the SpatialHashing
 
@@ -151,4 +152,4 @@ class SpatialHashing:
         :param fiber_system: list[list[Ball]]
         """
         for fiber in fiber_system:
-            self.add_fiber(fiber)
+            self.add_fiber(fiber, is_periodic)
