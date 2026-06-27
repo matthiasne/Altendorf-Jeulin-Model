7fa3e0032ed016ec4982521e55e2bb7cb583f243
Alex Keilmann <keilmann@rptu.de>
2026-06-27 07:53:40 +0200

    informally merge tests from main

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,21 +1,28 @@
 import unittest
+
 import numpy as np
-from Altendorf_Jeulin_Model.utils import periodic_distance, angle_between, normalized
+
+from Altendorf_Jeulin_Model.utils import (
+    angle_between,
+    cartesian_to_spherical,
+    normalized,
+    periodic_distance,
+    spherical_to_cartesian,
+    spherical_to_matrix,
+)
 
 
 class test_utils(unittest.TestCase):
     def test_periodic_distance(self):
         coord1 = np.array([30.0, 30.0, 30.0])
         coord2 = np.array([32.0, 30.0, 30.0])
-        coord3 = np.array([1., 1., 1.])
-        coord4 = np.array([63., 1., 1.])
+        coord3 = np.array([1.0, 1.0, 1.0])
+        coord4 = np.array([63.0, 1.0, 1.0])
         image_size = (64, 64, 64)
         dist, _ = periodic_distance(coord1, coord2, image_size)
-        self.assertEqual(dist, 2,
-                         "Error in periodic distance")
+        self.assertEqual(dist, 2, "Error in periodic distance")
         dist, _ = periodic_distance(coord3, coord4, image_size)
-        self.assertEqual(dist, 2,
-                         "Error in periodic distance")
+        self.assertEqual(dist, 2, "Error in periodic distance")
 
     def test_angle_between(self):
         v1 = np.array([1.0, 0.0, 0.0])
@@ -49,6 +56,56 @@ class test_utils(unittest.TestCase):
         l4, vl = normalized(v4)
         np.testing.assert_array_equal(vl, v4)
         self.assertEqual(l4, 0)
-        l5, vl = normalized(2*v1)
+        l5, vl = normalized(2 * v1)
         np.testing.assert_array_equal(vl, v1)
         self.assertEqual(l5, 2)
+
+    def test_cartesian_to_spherical(self):
+        r, theta, phi = cartesian_to_spherical(1, 0, 0)
+        self.assertAlmostEqual(r, 1)
+        self.assertAlmostEqual(theta, np.pi / 2.0)
+        self.assertAlmostEqual(phi, 0)
+        r, theta, phi = cartesian_to_spherical(0, 1, 0)
+        self.assertAlmostEqual(r, 1)
+        self.assertAlmostEqual(theta, np.pi / 2.0)
+        self.assertAlmostEqual(phi, np.pi / 2.0)
+        r, theta, phi = cartesian_to_spherical(0, 0, 2)
+        self.assertAlmostEqual(r, 2)
+        self.assertAlmostEqual(theta, 0)
+        self.assertAlmostEqual(phi, 0)
+        r, theta, phi = cartesian_to_spherical(0, 0, 0)
+        self.assertAlmostEqual(r, 0)
+        self.assertAlmostEqual(theta, 0)
+        self.assertAlmostEqual(phi, 0)
+
+    def test_spherical_to_cartesian(self):
+        x, y, z = spherical_to_cartesian(1, np.pi / 2.0, 0)
+        self.assertAlmostEqual(x, 1)
+        self.assertAlmostEqual(y, 0)
+        self.assertAlmostEqual(z, 0)
+        x, y, z = spherical_to_cartesian(1, np.pi / 2.0, np.pi / 2.0)
+        self.assertAlmostEqual(x, 0)
+        self.assertAlmostEqual(y, 1)
+        self.assertAlmostEqual(z, 0)
+        x, y, z = spherical_to_cartesian(2, 0, 0)
+        self.assertAlmostEqual(x, 0)
+        self.assertAlmostEqual(y, 0)
+        self.assertAlmostEqual(z, 2)
+        x, y, z = spherical_to_cartesian(0, 0, np.pi / 2.0)
+        self.assertAlmostEqual(x, 0)
+        self.assertAlmostEqual(y, 0)
+        self.assertAlmostEqual(z, 0)
+
+        x = 1
+        y = 2
+        z = 3
+        r, theta, phi = cartesian_to_spherical(x, y, z)
+        xt, yt, zt = spherical_to_cartesian(r, theta, phi)
+        self.assertAlmostEqual(xt, x)
+        self.assertAlmostEqual(yt, y)
+        self.assertAlmostEqual(zt, z)
+
+    def test_spherical_to_matrix(self):
+        mat = spherical_to_matrix(0, 0)
+        mat_test = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        np.testing.assert_array_equal(mat, mat_test)
